32781c685869f38442588b5a48cd315c5ba01a80
allseen <allseen@live.ru>
2022-08-13 14:10:47 +0300

    Lecture 3

---
+++ 03.Lecture/00/Program.cs
@@ -0,0 +1,24 @@
+﻿void Method1()
+{
+    Console.WriteLine("Method1");
+}
+Method1();
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Method2");
+
+void Method21(string msg, int count)
+{
+    for(int i = 0; i < count; i++)
+    Console.WriteLine(msg);
+}
+Method21(count: 1, msg:"Method21");
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+Console.WriteLine(Method3());
---
+++ 03.Lecture/01/Program.cs
@@ -0,0 +1,11 @@
+﻿string Method4(int count, string text)
+{
+    string result = string.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+Console.WriteLine(Method4(10, "z"));
