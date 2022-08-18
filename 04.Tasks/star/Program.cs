fbbed9b8c1fc6a139f3fb60cf45d0c63805e3164
allseen <allseen@live.ru>
2022-08-18 16:10:53 +0300

    negative value

--- 04.Tasks/star/Program.cs
+++ 04.Tasks/star/Program.cs
@@ -23,10 +23,17 @@
 Console.WriteLine("Enter array in one line seperating elements by space, comma or dot: ");
 string? str = Console.ReadLine();
 Console.Write("[");
+int sign = 1;
 for(int i = 0; i < str.Length; i++)
 {
+    sign = 1;
     while(str[i] == 44 || str[i] == 32 || str[i] == 46) i++;
-    if(str[i] <= 57 && str[i] >= 48) Console.Write(Fuct(str[i]-48));
+    if(str[i] == 45)
+    {
+    sign=-1; i++; 
+    }
+    else sign=1;
+    if(str[i] <= 57 && str[i] >= 48) Console.Write(Fuct((str[i]-48)*sign));
     if(i < str.Length-1) Console.Write(", ");
 }
 Console.Write("]");
