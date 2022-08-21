bb87e5f0c746d45c6e2a08c936e472936898a00b
allseen <allseen@live.ru>
2022-08-21 22:03:32 +0300

    double

--- 05.Tasks/38/Program.cs
+++ 05.Tasks/38/Program.cs
@@ -1,12 +1,14 @@
 ﻿int arrLen = new Random().Next(5,10);
-int[] arr = new int[arrLen];
-int min = 1000;
-int max = 0;
+double[] arr = new double[arrLen];
+double min = Double.MaxValue;
+double max = Double.MinValue;
+Console.WriteLine($"Enter a real number for all {arrLen} elements in array:");
 for (int i = 0; i < arr.Length; i++)
 {
-    arr[i] = new Random().Next(0,1000);
+    Console.Write($"Element {i} = ");
+    arr[i] = Convert.ToDouble(Console.ReadLine());
     if(arr[i] > max) max = arr[i];
     if(arr[i] < min) min = arr[i];
 }
-Console.Write($"[{string.Join(",", arr)}]");
+Console.Write($"[{string.Join("; ", arr)}]");
 Console.Write($" -> {max - min}");
