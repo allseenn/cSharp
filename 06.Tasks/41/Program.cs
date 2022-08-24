e76c0f8b45179d9d4ceea1c7acb3071be085e750
allseen <allseen@live.ru>
2022-08-24 19:28:04 +0300

    task 43

---
+++ 06.Tasks/41/Program.cs
@@ -0,0 +1,14 @@
+﻿int NumbersAboveZero()
+{
+Console.Write("Give a number or leave empty to exit: ");
+var M = Console.ReadLine();
+int count = 0;
+if (!string.IsNullOrEmpty(M))
+{
+    count= count + NumbersAboveZero();
+    if(Convert.ToInt32(M)> 0) count++;
+}
+return count;
+}
+
+Console.WriteLine($"{NumbersAboveZero()} numbers without zero.");
---
+++ 06.Tasks/43/Program.cs
@@ -0,0 +1,23 @@
+﻿double k1, k2, b1, b2, x, y = 0;
+Console.Write("Enter b1: ");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k1: ");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+k2 = Convert.ToDouble(Console.ReadLine());
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Match lines");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Parallel lines");
+}
+else
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+    Console.WriteLine($"Cross point is ({x}, {y})");
+}
